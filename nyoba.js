e5da487992a8ac7cd67506d3a2df91ba8d2b477e
Rodhi <Rodhi@gmail.com>
2022-09-26 10:30:37 +0700

    fix link ke home

---
+++ nyoba.js
@@ -0,0 +1,13 @@
+let nilai = 1
+
+switch (true) {
+    case (nilai >= 80):
+        console.log("A");
+        break;
+    case (nilai < 80 ):
+        console.log("B");
+        break;
+    default:
+        console.log("C");
+        break;
+}
